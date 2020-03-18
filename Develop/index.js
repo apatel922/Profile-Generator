1f0203e29d246b6848e7934f63cb97e3297c2f79
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-03-18 18:37:38 -0400

    first push

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,94 @@
-const questions = [
+// const questions = [
   
-];
+// ];
 
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
  
-}
+// }
 
-function init() {
+// function init() {
 
-init();
+// }
+
+// init();
+
+
+// const fs = require("fs");
+// const axios = require("axios");
+// const inquirer = require("inquirer");
+
+// inquirer.prompt([
+//     {
+//       type: "list",
+//       name: "color",
+//       message: "What is your favorite color?",
+//       choices: [
+//           "Red",
+//           "Blue",
+//           "Purple",
+//           "Orange",
+//           "Green"
+//       ]
+//     },
+//     {
+//       type: "input",
+//       name: "name",
+//       message: "What is your GitHub username?"
+//     }
+
+// ]).then(function(data) {
+//   let filename = data.name.toLowerCase().split(" ").join(" ") + ".json";
+//   fs.writeFile(filename, JSON.stringify(data, null, "\t"), function(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log("Success");
+//   });
+// }).then((response) => {
+//   console.log(response.name)
+//   axios.get(`https://api.github.com/users/${response.name}`)
+//     .then(data => {
+//       console.log(data);
+//       // data.data.login
+//       // data.data.avatar_url
+//       // data.data.email
+//       // Add badges
+//       // Write to file
+//       fs.writeFile("README.md", function(err) {
+//       })
+//     })
+// });
+
+
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+// username
+// project title
+// description of project
+// license ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+// command to install dep
+// command run tests
+// Instructions
+// How to contribute
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+    }
+  ]).then((response) => {
+    console.log(response.github)
+    axios.get(`https://api.github.com/users/${response.github}`)
+      .then(data => {
+        console.log(data);
+        // data.data.login
+        // data.data.avatar_url
+        // data.data.email
+        // Add badges
+        // Write to file
+        fs.writeFile("README.md", function(err) {
+        })
+      })
+  })
